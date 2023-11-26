8d84b545c3aab22e3111d21260bb40ced689d962
aaltoo <stanportableuse@gmail.com>
2023-11-26 18:24:51 +0900

    Add main layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArtworkListPageComponent } from './core/pages/artwork/list/artwork-list-page.component';
 import { ArtworkViewPageComponent } from './core/pages/artwork/view/artwork-view-page.component';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/artworks' },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/artworks',
+  },
   {
     path: 'artworks',
+    component: MainLayoutComponent,
     children: [
       { path: '', component: ArtworkListPageComponent },
       { path: ':id', component: ArtworkViewPageComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MainLayoutComponent,
+  ],
 })
 export class AppModule {}
