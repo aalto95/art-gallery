e50136a8eaa927622e7293cee6c165f09067dda4
aaltoo <stanportableuse@gmail.com>
2023-11-27 22:08:01 +0900

    Move layouts to app

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from 'src/wigdets/header/header.component';
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
 })
 export class MainLayoutComponent {}
